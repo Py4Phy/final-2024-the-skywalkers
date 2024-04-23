b8df5c5ee32ce37a9bccf04d85440ee0d6e79c43
Simon Tebeck <simtewil@gmail.com>
2024-04-22 23:46:45 -0700

    work on Earth orbit

--- Submission/final_project.py
+++ Submission/final_project.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 matplotlib.style.use("ggplot")
 import tqdm
 
+# integrators
+import integrators
+
 
 
 # =============================================================================
@@ -18,12 +21,20 @@ import tqdm
 # =============================================================================
 
 # gravitational constant
-G_gravity = 4*np.pi**2 # astronomical units
+G_gravity = 4*np.pi**2 *(1/365.256)**2 # astronomical units but per days not years
+yr = 365.256 # days
+
+# astronomical unit
+au =  1.495978e11 # m
+
+def AU(x): return x/au
+def AU_inv(x): return x*au
+
 
 # masses
 # SI
 m_sun = 1.9885E+30 # kg
-m_earth = 5.97E+24 # kg
+m_earth = 5.972E+24 # kg
 m_moon = 7.35E+22 # kg
 m_endor = 7.52E+23 # kg
 # m_bb =
@@ -33,31 +44,27 @@ m_endor = 7.52E+23 # kg
 mass = {'Sun': 1.,
         'Earth': m_earth/m_sun,
         'Moon': m_moon/m_sun,
-        'Endor': m_endor/m_sun}
+        'Endor': m_endor/m_sun
+        }
+
+# orbital period in Earth days
+period = {'Earth': 365.256,
+          'Moon': 1
+}
 
 
-# angular position in 1690 in degrees
-# theta0 = {'Uranus': 205.640,
-#          'Neptune': 288.380,
+# distance from the respective orbit center in AU
+distance = {'Earth': 1,
+            'Moon': 3.85e8 / au,
+}
+
 # }
-# # distance from the sun in AU
-# distance = {'Uranus': 19.1914,
-#             'Neptune': 30.0611,
-# }
-# # mass in AU
-# mass = {'Sun': 1.,
-#         'Uranus': 4.366244e-5,
-#         'Neptune': 5.151389e-5,
-# }
-# # orbital period in Earth years
-# period = {'Uranus': 84.0110,
-#           'Neptune': 164.7901,
-# }
+
 
 #------------------------------------------------------------
 
 
-def initial_position(angle, distance):
+def initial_position(distance, angle=0):
     """Calculate initial planet position.
 
     Parameters
@@ -75,28 +82,26 @@ def initial_position(angle, distance):
     x = np.deg2rad(angle)
     return distance * np.array([np.cos(x), np.sin(x)])
 
-def initial_velocity(angle, distance, period):
+
+def initial_velocity(distance, period, angle=0):
     # Convert angle from degrees to radians
     angle_rad = angle * (np.pi / 180)
 
-    velocity_au_year = 2 * np.pi * distance / period
+    velocity_au_day = 2 * np.pi * distance / period
 
     # Calculate x and y components of the velocity
-    vx = velocity_au_year * -np.sin(angle_rad)
-    vy = velocity_au_year * np.cos(angle_rad)
+    vx = velocity_au_day * -np.sin(angle_rad)
+    vy = velocity_au_day * np.cos(angle_rad)
 
-    return [vx, vy]
+    return np.array([vx, vy])
 
 
-# Calculate the initial velocities for Uranus and Neptune
-# key = 'Uranus'
-# initial_velocity_uranus = initial_velocity(theta0[key], distance[key], 
-#                                            period[key])
-# key = 'Neptune'
-# initial_velocity_neptune = initial_velocity(theta0[key], distance[key], 
-#                                             period[key])
+# Calculate the initial velocities for Earth
+key = 'Earth'
+initial_velocity_earth = initial_velocity(distance[key], 
+                                            period[key], angle=0)
 
-# initial_velocity_uranus, initial_velocity_neptune
+
 
 
 
@@ -149,106 +154,99 @@ def omega(v, r):
 
 
 # calculate the total force
-def F_total(r, coupled=True):
+def F_total(r):
+    m_Sun = mass['Sun']
+    m_Earth = mass['Earth']
     
-    rU = r[0]
-    rN = r[1]
-    mU = mass['Uranus']
-    mN = mass['Neptune']
-    mS = mass['Sun']
+    F_sun = F_gravity(r, m_Earth, m_Sun)
     
-    # force from Sun
-    F_sun_U = F_gravity(rU, mU, mS)
-    F_sun_N = F_gravity(rN, mN, mS)
+    return F_sun
     
-    if coupled:
-        # calculate only for Uranus, the one for Neptune is that one negative
-        F_other = F_gravity(rU - rN, mU, mN)
-    else:
-        F_other = np.array([0,0])
     
-    F_total = np.array([F_sun_U + F_other, F_sun_N - F_other])
-    return F_total
+# def F_total(r, coupled=True):
+    
+#     rU = r[0]
+#     rN = r[1]
+#     mU = mass['Uranus']
+#     mN = mass['Neptune']
+#     mS = mass['Sun']
+    
+#     # force from Sun
+#     F_sun_U = F_gravity(rU, mU, mS)
+#     F_sun_N = F_gravity(rN, mN, mS)
+    
+#     if coupled:
+#         # calculate only for Uranus, the one for Neptune is that one negative
+#         F_other = F_gravity(rU - rN, mU, mN)
+#     else:
+#         F_other = np.array([0,0])
+    
+#     F_total = np.array([F_sun_U + F_other, F_sun_N - F_other])
+#     return F_total
 
 
 # main algorithm: integrate the orbits
 def integrate_orbits(dt=0.1, t_max=160, coupled=True):
-    """Integrate equations of motion of Uranus and Neptune, including Sun's 
-    influence.
-
-    Parameters
-    ----------
-    dt : float
-       integrator timestep
-    t_max : float
-       integrate to t_max years
-    coupled : bool
-       * `True`: include the interaction between Neptune and Uranus
-       * `False`: no interaction (Uranus and Neptune move independently)
-
-    Returns
-    -------
-    time : array
-       array with the times for each step (in years)
-    r : array
-       positions of the planets, shape (N, 2, 2), where::
-          r[:, 0] = rU : Uranus x, y
-          r[:, 1] = rN : Neptune x, y
-    v : array
-       velocities of the planets, shape (N, 2, 2), where::
-          v[:, 0] = vU : Uranus vx, vy
-          v[:, 1] - vN : Neptune vx, vy
+    """Integrate equations of motion
     """
     nsteps = int(t_max/dt)
     time = dt * np.arange(nsteps)
 
-    # r array: time steps, 2 planets, 2 room coordinates
-    r = np.zeros((nsteps, 2, 2)) # [ [ [x_U_t1, y_U_t1], [x_N_t1, y_N_t1]], ...]
-    v = np.zeros_like(r)
-
-
+    # # r array: time steps, 2 planets, 2 room coordinates
+    # r = np.zeros((nsteps, 2, 2)) # [ [ [x_U_t1, y_U_t1], [x_N_t1, y_N_t1]], ...]
+    # v = np.zeros_like(r)
+    
     # r[0, 0, :] = initial_position(theta0['Uranus'], distance['Uranus'])
     # r[0, 1, :] = initial_position(theta0['Neptune'], distance['Neptune'])
     # v[0, 0, :] = initial_velocity(theta0['Uranus'], distance['Uranus'], period['Uranus'])
     # v[0, 1, :] = initial_velocity(theta0['Neptune'], distance['Neptune'], period['Neptune'])
 
-    mU = mass['Uranus']
-    mN = mass['Neptune']
+    
+    r = np.zeros((nsteps, 2)) # [x1,y1],[x2,y2],...
+    v = np.zeros_like(r)
+
+    r[0,:] = initial_position(distance['Earth'])
+    v[0,:] = initial_velocity(distance['Earth'], period['Earth'])
+    
+
+    m_Earth = mass['Earth']
 
     # integration verlocity verlet
-    Ft = F_total(r[0], coupled=coupled)
+    Ft = F_total(r[0])
     for i in range(nsteps-1):
-        m = np.array([[mU, mU], [mN, mN]])
+        m = np.array([m_Earth, m_Earth])
         vhalf = v[i] + 0.5 * dt * Ft / m
         r[i+1] = r[i] + dt * vhalf
     
         # new force
-        Ft = F_total(r[i+1], coupled=coupled)
+        Ft = F_total(r[i+1])
         v[i+1] = vhalf + 0.5 * dt * Ft / m
 
     return time, r, v
 
 
+time, r, v = integrate_orbits(dt=1e-1, t_max=1*yr)
+plt.plot(r[:,0], r[:,1])
+plt.axis('equal')
 
 
 
 
-# Simulating orbits WITHOUT interactions
-print("Simulating Uranus and Neptune orbits WITHOUT interactions")
-time, r0, v0 = integrate_orbits(t_max=160, coupled=False)
-rU0 = r0[:, 0]  # Uranus position without interaction
-vU0 = v0[:, 0]  # Uranus velocity without interaction
-omegaU0 = omega(vU0, rU0)  # Angular velocity of Uranus without interaction
+# # Simulatirint("Simulating Uranus and Neptune orbits WITHOUT interactions")
+# time, r0, v0 = integrate_orbits(t_max=160, coupled=False)
+# rU0 = r0[:, 0]  # Uranus position without interaction
+# vU0 = v0[:, 0]  # Uranus velocity without interaction
+# omegaU0 = omega(vU0, rU0)  # Angular velocity of Uranus without interaction
 
-# Simulating orbits WITH interactions
-print("Simulating Uranus and Neptune orbits WITH interactions")
-time, r, v = integrate_orbits(t_max=160, coupled=True)
-rU = r[:, 0]  # Uranus position with interaction
-vU = v[:, 0]  # Uranus velocity with interaction
-omegaU = omega(vU, rU)  # Angular velocity of Uranus with interaction
+# # Simulating orbits WITH interactions
+# print("Simulating Uranus and Neptune orbits WITH interactions")
+# time, r, v = integrate_orbits(t_max=160, coupled=True)
+# rU = r[:, 0]  # Uranus position with interaction
+# vU = v[:, 0]  # Uranus velocity with interaction
+# omegaU = omega(vU, rU)  # Angular velocity of Uranus with interaction
 
-# Calculate DeltaOmega for Uranus
-DeltaOmegaU = omegaU - omegaU0
+# # Calculate DeltaOmega for Uranus
+# DeltaOmegaU = omegaU - omegaU0
 
 # # Plotting the orbits
 # plt.figure(figsize=(12, 6))
@@ -275,7 +273,8 @@ DeltaOmegaU = omegaU - omegaU0
 # plt.tight_layout()
 # plt.savefig('uranus_neptune_orbits.png')
    
-
+# ng orbits WITHOUT interactions
+# p
 # # plot of delta omega
 # plt.figure()
 # plt.plot(time, DeltaOmegaU)
